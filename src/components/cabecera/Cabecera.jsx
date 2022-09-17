e02f679e41f9ed6339b65007c8a60f56985a2054
cristian <cristian192003@outlook.es>
2022-09-17 15:42:32 -0500

    aqui empezamos con el Oziel

--- src/components/cabecera/Cabecera.jsx
+++ src/components/cabecera/Cabecera.jsx
@@ -1,28 +1,13 @@
 import style from "./cabecera.module.css";
 //import logo from "../../utils/img/HM logo3.png";
 import { Logo } from "./logo/Logo";
-import { useState } from "react";
 
 export function Cabecera() {
-  const [click, setClick] = useState(false);
-
-  const display = () => {
-    if (window.screen.width < 1000) {
-      console.log(window.screen.width);
-      setClick(true);
-    } else {
-      setClick(false);
-    }
-  };
   return (
     <div className={style.contenedor}>
       <div className={style.izq}></div>
       <div className={style.espacio}></div>
-      {/* <div className={style.logo}>
-        <img src={logo} alt="" />
-      </div> */}
-      {/* {click ? <Logo/> : <h2>HOla</h2>} */}
-      <Logo/>
+    <Logo/>
       <div className={style.conDer}>
         <div className={style.der}></div>
       </div>
