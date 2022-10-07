c6448cc1b4ac2da739d400f6770bc73c4ed07a10
cristian <cristian192003@outlook.es>
2022-10-07 17:51:45 -0500

    Se agregaron librerias nuevas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"; //Este es para poder hacer interfaces
 import ReactDOM from "react-dom/client"; //Esta para hacer interfaces con el DOM
+import { ActImg } from "./components/actImg/ActImg";
 import { Cabecera } from "./components/cabecera/Cabecera";
 import { Carrucel } from "./components/Carrucel/Carrucel";
 //import { Carrucel } from "./components/carrucel/Carrucel";
@@ -8,6 +9,6 @@ import "./normalize.css"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<>
 <Cabecera/>
-
 <Carrucel/>
+<ActImg/>
 </>);
