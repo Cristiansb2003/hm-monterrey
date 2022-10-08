8757ef3fe2dcd9d5f928aeea91f1e035ddce5e2a
cristian <cristian192003@outlook.es>
2022-10-07 20:54:03 -0500

    Version de prueba XIomara

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react"; //Este es para poder hacer interfaces
 import ReactDOM from "react-dom/client"; //Esta para hacer interfaces con el DOM
-import { ActImg } from "./components/actImg/ActImg";
 import { Cabecera } from "./components/cabecera/Cabecera";
 import { Carrucel } from "./components/Carrucel/Carrucel";
 //import { Carrucel } from "./components/carrucel/Carrucel";
@@ -10,5 +9,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<>
 <Cabecera/>
 <Carrucel/>
-<ActImg/>
+<h1>Hola</h1>
 </>);
